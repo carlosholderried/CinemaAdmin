556e4ceae53b86b1c97e1eec7724b222e5af0211
Carlos Holderried <93011560+carlosholderried@users.noreply.github.com>
2023-03-23 13:34:21 -0300

    Add files via upload

---
+++ src/components/HomePage.vue
@@ -0,0 +1,37 @@
+<template>
+  <body>
+      <HomeHeader />
+      <h1>Bem vindo ao sistema de cadastro</h1>
+      <img src="https://media.tenor.com/bCfpwMjfAi0AAAAC/cat-typing.gif" alt="">
+      
+  </body>
+</template>
+
+<script>
+import HomeHeader from './HomeHeader.vue';
+import axios from 'axios';
+axios.baseURL = '';  // atribui o ip da api ao baseURL
+export default{
+    name:'HomePage',
+    data(){
+      return{
+            }
+    },
+    components:{
+      HomeHeader
+},
+    methods:{
+                              
+    },
+    mounted()
+    {        
+        
+    }
+}
+</script>
+
+<style>
+body {
+  background-color: rgb(54, 54, 54);
+}
+</style>
